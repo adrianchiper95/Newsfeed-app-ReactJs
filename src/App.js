aba052e4ddec7bb99693905e33f5ab4566625d36
adrianchiper95 <adrian.chiper@hotmail.com>
2019-06-22 21:21:24 +0300

    added footer section and minor changes in design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Components/Header';
 import Content from './Components/Content';
+import Footer from './Components/Footer'
 
 const App = () => {
     return (
         <div>
             <Header />
             <Content />
+            <Footer />
         </div>
     );
 };
