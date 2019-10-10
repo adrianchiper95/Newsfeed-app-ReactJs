6ded426b480bfc88c1fad448a063d30f8fb8a3d8
adrianchiper95 <adrian.chiper@hotmail.com>
2019-10-11 01:30:23 +0300

    changed on of the sources

--- src/Components/Api.js
+++ src/Components/Api.js
@@ -1,4 +1,4 @@
 export const api = 'https://newsapi.org/v2/everything?language=en&q=';
-export const sources = ['aerojet', 'airbus helicopters', 'nasa', 'spacex', 'aerospace', 'aurora'];
+export const sources = ['aerojet', 'airbus helicopters', 'nasa', 'spacex', 'aerospace', 'the planetary society'];
 export const apiKey = '&apiKey=c502aef5b4174ef8ac3cdb0d3ef34c9d';
 export const defaultSource = 'space science';
