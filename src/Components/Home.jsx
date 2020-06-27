334fca7f7676b9a7239f3fd13263ac2ab388a8e1
adrianchiper95 <adrian.chiper@hotmail.com>
2020-06-28 02:25:40 +0300

    update: removed login and contact section

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import './Header.css';
 import './Content.css';
 import logo from './images/space_logo.png';
-import loginLogo from './images/login_logo.png';
-import contactLogo from './images/contact_logo.png';
 
 class Home extends Component {
     render() {
@@ -16,20 +14,6 @@ class Home extends Component {
             </div>
             <div className="logo_text">
             <h1>Void.<span >News</span></h1>
-            </div>
-            </div>
-            <div className="section">
-            <div className="login_text">
-            <h3>Login</h3>
-            </div>
-            <div className="login_logo">
-            <img src={loginLogo} alt="Login"/>
-            </div>
-            <div className="contact_text">
-            <h3>Contact</h3>
-            </div>
-            <div className="contact_logo">
-            <img src={contactLogo} alt="Contact"/>
             </div>
             </div>
             </header>
